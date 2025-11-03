3d2ddd4d2c6e97fc385b0ae57de5a4e84c03a7ab
Abhash Pandey <abhashkrpandey4@gmail.com>
2025-11-03 11:16:14 +0530

    fixed the add button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,32 +15,33 @@ export default function Home({ refreshTasks, taskList, filter }) {
   }
   return (
     <>
-      <div className="flex flex-row justify-between  p-4 gap-6 bg-gray-50 rounded-lg shadow-sm">
-        <div className=" flex-1 h-dvh overflow-y-auto pb-24">
-          <TaskList    
+      <div className="flex flex-col justify-between  p-4 gap-6  rounded-lg ">
+        <div className=" flex-1 h-dvh  pb-5">
+          <TaskList
             tasks={taskList}
             refreshTasks={refreshTasks}
             filter={filter}
           />
         </div>
+        <div className="flex flex-row-reverse  bottom-0 right-5 sticky">
+          <div className="w-[100px] sm:w-[150px] md:w-[300px] flex flex-col items-end gap-3">
+            <button
+              className={`p-2 rounded-md text-white ${
+                hasToAdd
+                  ? "bg-red-500 hover:bg-red-600"
+                  : "bg-green-600 hover:bg-green-700"
+              } transition`}
+              onClick={taskAdder}
+            >
+              {hasToAdd ? <X /> : <Plus />}
+            </button>
 
-        <div className="w-[100px] sm:w-[150px] md:w-[300px] flex flex-col items-end gap-3">
-          <button
-            className={`p-2 rounded-md text-white ${
-              hasToAdd
-                ? "bg-red-500 hover:bg-red-600"
-                : "bg-green-600 hover:bg-green-700"
-            } transition`}
-            onClick={taskAdder}
-          >
-            {hasToAdd ? <X /> : <Plus />}
-          </button>
-
-          {hasToAdd && (
-            <div className="w-full">
-              <TaskForm refreshTasks={refreshTasks} />
-            </div>
-          )}
+            {hasToAdd && (
+              <div className="w-full">
+                <TaskForm refreshTasks={refreshTasks} />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
